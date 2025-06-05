c7dfc0575a215f27ecac422fc732d41ca3d43785
MaraCity <ZeSigmaNoam@gmail.com>
2025-06-05 14:21:08 +0300

    Flappy-Bird 2.0

---
+++ download web/script.js
@@ -0,0 +1,7 @@
+// JavaScript בסיסי – נוכל להוסיף כאן פונקציות בעתיד אם תרצה
+
+// לדוגמה: הודעה בעת לחיצה על כפתור ההורדה
+const downloadBtn = document.querySelector('.btn-download');
+downloadBtn.addEventListener('click', () => {
+    alert('תודה שהורדת את Flappy Bird! בהצלחה במשחק 🎮');
+});
